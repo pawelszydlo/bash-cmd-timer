1d104ba817e8b55b25063d778d5e333f92fd7839
Paweł Szydło <pawelszydlo@gmail.com>
2015-10-09 20:42:56 +0200

    Refactoring and compatibility fixes.

--- README.md
+++ README.md
@@ -4,12 +4,28 @@ Display a timer in the upper-right hand side of your console, timing current com
 ![screenshot](http://i.imgur.com/vcHKBgf.png)
 
 ###Quick Start
+Download and install the script:
 ```bash
-# Download the script:
 curl https://raw.githubusercontent.com/pawelszydlo/bash-cmd-timer/master/cmd-timer.sh -o ~/.cmd-timer.sh
-# Install:
 source ~/.cmd-timer.sh install
 ```
+
+####Features
+* No effort required - install and it just works
+* Highly configurable
+* Live timer during command execution
+* Summary after execution
+* Logging of execution times
+* Compatible with Linux and OS X
+
+
+####Configuration 
+Configuration can be done by editing variables at the beginning of the script or by overwriting them through user config.
+The script will look for user config in:
+```
+~/.cmd-timer-conf.sh
+```
+
 
 ###Uninstall
 ```bash
@@ -24,7 +40,5 @@ The timer is started by the DEBUG trap and stopped throught PROMPT_COMMAND.
 ```bash
 trap - DEBUG
 ```
-
-###TODO
-* Determine the correct file to append the startup items to (.bashrc? .bash_profile? .profile? .bash_login?)
-* Logging
+####TODO
+* Better way to display live timer.
--- cmd-timer.sh
+++ cmd-timer.sh
@@ -4,135 +4,245 @@
 # https://github.com/pawelszydlo/bash-cmd-timer
 #
 # Author: Pawel Szydlo (pawelszydlo@gmail.com)
+_CMDT_LAST_CMD=$_
 
 
-# Configuration
-INSTALL_DEST="$HOME/.cmd-timer.sh"  # Where to install the script.
-DELAY=2  # Delay before showing the timer, in seconds.
-EXCLUDE=( vi vim less emacs )  # Do not time these commands.
-STARTUP_FILE="$HOME/.profile"  # File to which setup should be appended.
+###### CONFIGURATION ######
+
+
+# Where to install the script.
+_CMDT_INSTALL_DEST="$HOME/.cmd-timer.sh"
+# Minimum time before starting the timer, in seconds.
+_CMDT_DELAY=3
+# Print the summary after the command finishes.
+_CMDT_SUMMARY=false
+# Enable logging of execution time to file.
+_CMDT_LOGGING=true
+# Log file.
+_CMDT_LOGFILE="$HOME/.cmd-timer.log"
+# Do not time these commands. Arguments are matched if present, from the left.
+_CMDT_EXCLUDE=( vi vim less emacs pv "tail -f" ssh telnet scp ftp )
+
+
+###### HELPERS ######
+
+
+# Colour definitions.
+YELLOW="\033[1;33m"
+GREEN="\033[1;32m"
+WHITE="\033[1;37m"
+RED="\033[1;31m"
+PURPLE="\033[1;35m"
+NOCOL="\033[0m"
+
+
+###### FUNCTION DEFINITIONS ######
+
+
+# Main.
+_cmdt_main ()
+{
+    # Check if script is being sourced, not run.
+    if [[ $_CMDT_LAST_CMD = $0 ]]; then
+        echo "This script uses global env variables and must be sourced. Run:"
+        echo "source $0"
+        exit 1
+    fi
+
+    # Load user config, if exists.
+    if [ -r ~/.cmd-timer-conf.sh ]; then
+        source ~/.cmd-timer-conf.sh
+    fi
+
+    # Handle parameters
+    case "$1" in
+        start)
+            _cmdt_start ${@:2}
+            ;;
+        stop)
+            _cmdt_stop
+            ;;
+        install)
+            _cmdt_install
+            ;;
+        uninstall)
+            _cmdt_uninstall
+            ;;
+        *)
+            echo $"Usage: source $BASH_SOURCE {install|uninstall}"
+            return
+    esac
+}
+
+
+# Get the correct profile file to install to.
+_cmdt_get_profile () {
+    if [[ ! $(shopt -q login_shell) ]]; then
+        # It is a login shell.
+        if [ -f "$HOME/.bash_profile" ]; then
+            local profile_file="$HOME/.bash_profile"
+        else
+            local profile_file="$HOME/.profile"
+        fi
+    else
+        # It is not a login shell.
+        local profile_file="$HOME/.bashrc"
+    fi
+    echo $profile_file
+}
 
 
 # Print text in the top-right corner.
-__print ()
+_cmdt_print ()
 {
     tput sc  # Save the cursor
     tput cup 0 $((`tput cols` - ${#1} + 16))  # Move to upper right corner
-    echo -ne " $1 \r"
+    echo -ne " $1 "
     tput rc  # Restore cursor
 } 
 
 
 # Get elapsed time.
-__get_time ()
+_cmdt_get_time ()
 {
-    seconds="$((`date +%s` - $CMDTIMER_START))";
-    text=`printf '%02d:%02d:%02d' \
+    local seconds="$((`date +%s` - $_CMDT_TIMER_START))";
+    local text=`printf '%02d:%02d:%02d' \
         $(($seconds / 3600)) $(($seconds % 3600 / 60)) $(($seconds % 60))`
     echo "$text"
 }
 
 
 # Run the timer.
-__timer_start ()
+_cmdt_timer_start ()
 {
     # Wait before starting.
-    sleep $DELAY
+    sleep $_CMDT_DELAY
     # Check if command name was passed.
     if [ ! -z $1 ]; then
-        cmd="$1: "
+        local cmd="$1: "
     else
-        cmd=''
+        local cmd=''
     fi
     # Run until stopped.
     while true
     do
-        sleep 0.1
-        __print "\033[1;33m$cmd$(__get_time)\033[0m"
+        sleep 0.1 
+        _cmdt_print "$YELLOW$cmd$(_cmdt_get_time)$NOCOL"
     done
 }
 
 
-# Start the timer.
-start ()
+# Print the summary.
+_cmdt_summary ()
 {
-    # Check for excluded commands.
-    for item in "${EXCLUDE[@]}"; do
-        if [[ $1 == "$item" ]]; then
-            return
-        fi
-    done
+    echo -e "$PURPLE[ $_CMDT_TIMER_COMMAND ] executed in $1$NOCOL"
+}
 
+
+# Save the log.
+_cmdt_log ()
+{
+    echo "$_CMDT_TIMER_START;$1;$2;${@:3}" >> $_CMDT_LOGFILE
+}
+
+
+# Start the timer.
+_cmdt_start ()
+{
     # Start only if no PID var found.
-    if [ -z "$CMDTIMER_PID" ]; then
+    if [ -z "$_CMDT_TIMER_PID" ]; then
+        # Check for excluded commands.
+        for item in "${_CMDT_EXCLUDE[@]}"; do
+            for i in $(seq 1 ${#@}); do 
+                if [[ "${@:1:$i}" == "$item" ]]; then
+                    return
+                fi
+            done
+        done
         # Start the timer in the background and remember the PID.
-        export CMDTIMER_START=`date +%s`
-        __timer_start $1 &
-        export CMDTIMER_PID=$!
+        export _CMDT_TIMER_START=`date +%s`
+        export _CMDT_TIMER_COMMAND="$@"
+        _cmdt_timer_start $1 & disown
+        export _CMDT_TIMER_PID=$!
     fi
 }
 
 
 # Finish the timer.
-stop ()
+_cmdt_stop ()
 {
     # Stop only if PID var found.
-    if [ ! -z "$CMDTIMER_PID" ]; then
-        kill $CMDTIMER_PID 2> /dev/null
-        wait $CMDTIMER_PID 2> /dev/null
-        # Check if the timer actually started and make the counter green.
-        seconds="$((`date +%s` - $CMDTIMER_START))";
-        if [ $seconds -gt $DELAY ]; then
-            __print "\033[1;32m$(__get_time)\033[0m"
+    if [ ! -z "$_CMDT_TIMER_PID" ]; then
+        kill $_CMDT_TIMER_PID 2> /dev/null
+        wait $_CMDT_TIMER_PID 2> /dev/null
+        
+        # Check if the timer actually started.
+        local seconds="$((`date +%s` - $_CMDT_TIMER_START))";
+        if [ $seconds -gt $_CMDT_DELAY ]; then
+            local elapsed=$(_cmdt_get_time)
+            # Print the summary if enabled.
+            if [ "$_CMDT_SUMMARY" = true ]; then
+                _cmdt_summary $elapsed
+            fi
+            # Save log if enabled
+            if [ "$_CMDT_LOGGING" = true ]; then
+                _cmdt_log $elapsed $_CMDT_TIMER_COMMAND
+            fi
+            _cmdt_print "$GREEN$elapsed$NOCOL"
+
         fi
-        unset CMDTIMER_PID
-        unset CMDTIMER_START
+        unset _CMDT_TIMER_PID
+        unset _CMDT_TIMER_COMMAND
+        unset _CMDT_TIMER_START
     fi
 }
 
+
 # Install the handlers.
-install ()
+_cmdt_install ()
 {
-    echo "Copying script to $INSTALL_DEST..."
-    cp $BASH_SOURCE $INSTALL_DEST
+    local profile_file="$(_cmdt_get_profile)"
+    echo "Copying script to $_CMDT_INSTALL_DEST..."
+    cp $BASH_SOURCE $_CMDT_INSTALL_DEST
 
     # This mechanism is highly unreliable, hence the checks when
     # starting/stopping the timer.
     echo "Setting handlers..."
     # Execute before each command
-    trap 'source '$INSTALL_DEST' start $BASH_COMMAND' DEBUG
+    trap 'source '$_CMDT_INSTALL_DEST' start $BASH_COMMAND' DEBUG
     # This command will be executed before each prompt, which means after
     # execution is finished.
-    export PROMPT_COMMAND="source $INSTALL_DEST stop"
+    export PROMPT_COMMAND="source $_CMDT_INSTALL_DEST stop"
 
-    # TODO: which file should be modified?
-    echo "Adding timer handlers to $STARTUP_FILE..."
-    echo "trap 'source "$INSTALL_DEST" start \$BASH_COMMAND' DEBUG" >> $STARTUP_FILE
-    echo 'export PROMPT_COMMAND="source '$INSTALL_DEST' stop"' >> $STARTUP_FILE
+    echo "Adding timer handlers to $profile_file..."
+    echo "trap 'source "$_CMDT_INSTALL_DEST" start \$BASH_COMMAND' DEBUG" >> $profile_file
+    echo 'export PROMPT_COMMAND="source '$_CMDT_INSTALL_DEST' stop"' >> $profile_file
 
     echo "Done."
 }
 
 
 # Uninstall everything.
-uninstall ()
+_cmdt_uninstall ()
 {
+    local profile_file="$(_cmdt_get_profile)"
     echo "Removing the handlers..."
     trap - DEBUG  # This is not working! I have no idea why.
-    stop
+    _cmdt_stop
 
     echo "Removing variables..."
     unset PROMPT_COMMAND
-    unset CMDTIMER_PID
-    unset CMDTIMER_START
+    unset _CMDT_TIMER_PID
+    unset _CMDT_TIMER_COMMAND
+    unset _CMDT_TIMER_START
 
-    echo "WARNING: All lines containing the string \"$INSTALL_DEST\" will be removed from $STARTUP_FILE. Press y to continue."
+    echo -e "\nWARNING:\nAll lines containing \"$_CMDT_INSTALL_DEST\" will be removed from $profile_file. Press y to continue."
     read -p "" -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         # Remove all lines containing the installation path.
-        sed -i.ctbak "/$(echo $INSTALL_DEST | sed -e 's/[\/&]/\\&/g')/d" $STARTUP_FILE
-        rm ${STARTUP_FILE}.ctbak
+        sed -i.ctbak "/$(echo $_CMDT_INSTALL_DEST | sed -e 's/[\/&]/\\&/g')/d" $profile_file
+        rm ${profile_file}.ctbak
     else
         echo "Skipping cleanup."
     fi
@@ -140,28 +250,8 @@ uninstall ()
 }
 
 
-# Check if script is being sourced, not run.
-if [[ $_ = $0 ]]; then
-    echo "This script uses global env variables and must be sourced. Run:"
-    echo "source $0"
-    exit 1
-fi
+###### RUN ######
 
 
-# Hnadle parameters.
-case "$1" in
-    start)
-        start $2
-        ;;
-    stop)
-        stop
-        ;;
-    install)
-        install
-        ;;
-    uninstall)
-        uninstall
-        ;;
-    *)
-        echo $"Usage: source $BASH_SOURCE {install|uninstall}"
-esac
+# Run the script.
+_cmdt_main $@
